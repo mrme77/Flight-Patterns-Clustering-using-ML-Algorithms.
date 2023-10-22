1650d17d287e799cb59d4f5c318b1b4b21b13103
Pasquale <137796364+mrme77@users.noreply.github.com>
2023-10-23 00:54:39 +0200

    Update adsb_flight_data.py

--- adsb_flight_data.py
+++ adsb_flight_data.py
@@ -13,15 +13,12 @@ config.read('config.ini')
 piaware_ip = config.get('PiAware', 'IP')
 piaware_port = config.getint('PiAware', 'Port')
 
-
-# Create a SQLite database and define flight_data and navigation tables
+# Create a SQLite database and define flight_data and navigation table
 def create_database():
-    conn = sqlite3.connect('flight_data.db')  
+    conn = sqlite3.connect('flight_data.db')  # Replace with the desired database name
     cursor = conn.cursor()
 
     # Create the flight_data table
-    #cursor.execute(''' DROP TABLE IF EXISTS flight_data''')
-    #cursor.execute(''' DROP TABLE IF EXISTS navigation_data''')
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS flight_data (
             id INTEGER PRIMARY KEY,
@@ -34,7 +31,8 @@ def create_database():
             longitude REAL
         )
     ''')
-    
+
+    # Create the navigation_data table
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS navigation_data (
             id INTEGER PRIMARY KEY,
@@ -44,13 +42,13 @@ def create_database():
             timestamp TEXT,
             speed REAL,
             heading REAL
-            
         )
     ''')
+
     conn.commit()
     conn.close()
 
-
+# Create the database and flight_data table
 create_database()
 
 # Handle Ctrl+C to gracefully exit the program
@@ -58,63 +56,61 @@ def signal_handler(signal, frame):
     print("Exiting the program.")
     sys.exit(0)
 
-
 # Create a function to handle the data stream and database operations
 def data_stream_and_store():
-    # Connect to the SQLite database
-    conn = sqlite3.connect('flight_data.db')
-    cursor = conn.cursor()
+    while True:
+        try:
+            # Connect to the SQLite database
+            conn = sqlite3.connect('flight_data.db')
+            cursor = conn.cursor()
 
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.settimeout(1200)  
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.settimeout(600)
 
-    try:
-        sock.connect((piaware_ip, piaware_port))
-        print('Connection established')
-
-        while True:
             try:
-                data = sock.recv(4096)
-                if not data:
-                    print("No data received.")
-                    break
+                sock.connect((piaware_ip, piaware_port))
+                print('Connection established')
 
-                data_str = data.decode('utf-8')
-                fields = data_str.split(',')
-                
-                
-                # Handle the case where the MSG message type is not 3 or 4
-                if not fields[0] == 'MSG' or not (fields[1] == '3' or fields[1] == '4'):
-                    continue
+                while True:
+                    try:
+                        data = sock.recv(4096)
+                        if not data:
+                            print("No data received.")
+                            continue  # Continue receiving data
 
+                        data_str = data.decode('utf-8')
+                        fields = data_str.split(',')
 
-                # Insert the data into the correct table
-                if fields[1] == '3' and len(fields)>20:
-                   
-                   cursor.execute("INSERT INTO flight_data (message_type, aircraft_icao_id, date, timestamp, altitude, latitude, longitude) VALUES (?, ?, ?, ?, ?, ?, ?)",
-                  (fields[0]+fields[1], fields[4], fields[6], fields[7], fields[11], fields[14], fields[15]))
-                   print(f"Record added - Message Type: {fields[0]+fields[1]}, Aircraft: {fields[4]}, Altitude: {fields[11]}, Latitude: {fields[14]}, Longitude: {fields[15]}")
-                
-                elif fields[1] == '4'and len(fields)>20:
-                     
-                     cursor.execute("INSERT INTO navigation_data (message_type, aircraft_icao_id, date, timestamp, speed, heading) VALUES (?, ?, ?, ?, ?, ?)",
-                  (fields[0]+fields[1], fields[4], fields[6], fields[7], fields[12], fields[13]))
-                     print(f"Record added - Message Type: {fields[0]+fields[1]}, Aircraft: {fields[4]}, Speed: {fields[12]}, Heading: {fields[13]}")
-                # Commit the changes to the database
-                conn.commit()
-                time.sleep(10)
+                        # Handle the case where the MSG message type is not 3 or 4
+                        if not fields[0] == 'MSG' or not (fields[1] == '3' or fields[1] == '4'):
+                            continue
 
-            except socket.timeout:
-                print("Socket timeout, waiting for data...")
-                time.sleep(10)
-            except Exception as e:
-                print(f"Error: {e}")
-    except socket.error as e:
-        print(f"Socket error: {e}")
-    finally:
-        cursor.close()
-        conn.close()
-        sock.close()
+                        # Insert the data into the correct table
+                        if fields[1] == '3' and len(fields) > 20:
+                            cursor.execute("INSERT INTO flight_data (message_type, aircraft_icao_id, date, timestamp, altitude, latitude, longitude) VALUES (?, ?, ?, ?, ?, ?, ?)",
+                                           (fields[0] + fields[1], fields[4], fields[6], fields[7], fields[11], fields[14], fields[15]))
+                            print(f"Record added - Message Type: {fields[0] + fields[1]}, Aircraft: {fields[4]}, Altitude: {fields[11]}, Latitude: {fields[14]}, Longitude: {fields[15]}")
+                        elif fields[1] == '4' and len(fields) > 20:
+                            cursor.execute("INSERT INTO navigation_data (message_type, aircraft_icao_id, date, timestamp, speed, heading) VALUES (?, ?, ?, ?, ?, ?)",
+                                           (fields[0] + fields[1], fields[4], fields[6], fields[7], fields[12], fields[13]))
+                            print(f"Record added - Message Type: {fields[0] + fields[1]}, Aircraft: {fields[4]}, Speed: {fields[12]}, Heading: {fields[13]}")
+
+                        # Commit the changes to the database
+                        conn.commit()
+                        time.sleep(10)
+                    except socket.timeout:
+                        print("Socket timeout, waiting for data...")
+                        time.sleep(10)
+                    except Exception as e:
+                        print(f"Error: {e}")
+            except socket.error as e:
+                print(f"Socket error: {e}")
+            finally:
+                cursor.close()
+                conn.close()
+                sock.close()
+        except Exception as e:
+            print(f"Outer Error: {e}")
 
 # Start the data stream and database operations
 if __name__ == "__main__":
