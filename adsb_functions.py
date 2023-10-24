e3b748a8fe6b7f81725d9b665615b7ba5d949d0d
Pasquale <137796364+mrme77@users.noreply.github.com>
2023-10-24 04:39:11 +0200

    Update adsb_functions.py

--- adsb_functions.py
+++ adsb_functions.py
@@ -1,103 +1,92 @@
+import sqlite3
+import socket
+import configparser
+import time
 
+
+# Create a SQLite database and define flight_data and navigation tables
 def create_database():
-    conn = sqlite3.connect('flight_data.db')  # Replace with the desired database name
-    cursor = conn.cursor()
+    with sqlite3.connect('flight_data.db') as conn:
+        cursor = conn.cursor()
 
-    # Create the flight_data table
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS flight_data (
-            id INTEGER PRIMARY KEY,
-            message_type TEXT,
-            aircraft_icao_id TEXT,
-            date TEXT,
-            timestamp TEXT,
-            altitude REAL,
-            latitude REAL,
-            longitude REAL
-        )
-    ''')
+        # Create the flight_data and navigation_data tables using a single loop
+        for table_name in ['flight_data', 'navigation_data']:
+            cursor.execute(f'''
+                CREATE TABLE IF NOT EXISTS {table_name} (
+                    id INTEGER PRIMARY KEY,
+                    message_type TEXT,
+                    aircraft_icao_id TEXT,
+                    date TEXT,
+                    timestamp TEXT,
+                    altitude REAL,
+                    latitude REAL,
+                    longitude REAL,
+                    speed REAL,
+                    heading REAL
+                )
+            ''')
 
-    # Create the navigation_data table
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS navigation_data (
-            id INTEGER PRIMARY KEY,
-            message_type TEXT,
-            aircraft_icao_id TEXT,
-            date TEXT,
-            timestamp TEXT,
-            speed REAL,
-            heading REAL
-        )
-    ''')
-
-    conn.commit()
-    conn.close()
-
-# Create the database and flight_data table
-create_database()
-
-# Handle Ctrl+C to gracefully exit the program
-def signal_handler(signal, frame):
-    print("Exiting the program.")
-    sys.exit(0)
+        conn.commit()
 
 # Create a function to handle the data stream and database operations
 def data_stream_and_store():
     while True:
         try:
             # Connect to the SQLite database
-            conn = sqlite3.connect('flight_data.db')
-            cursor = conn.cursor()
+            with sqlite3.connect('flight_data.db') as conn:
+                cursor = conn.cursor()
 
-            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            sock.settimeout(600)
+                sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                sock.settimeout(600)
 
-            try:
-                sock.connect((piaware_ip, piaware_port))
-                print('Connection established')
+                try:
+                    sock.connect((piaware_ip, piaware_port))
+                    print('Connection established')
 
-                while True:
-                    try:
+                    while True:
                         data = sock.recv(4096)
+
                         if not data:
                             print("No data received.")
-                            continue  # Continue receiving data
+                            print('Restarting socket')
+                            sock.close()
+                            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                            sock.settimeout(600)
+                            sock.connect((piaware_ip, piaware_port))
+                            continue
 
                         data_str = data.decode('utf-8')
                         fields = data_str.split(',')
 
-                        # Handle the case where the MSG message type is not 3 or 4
-                        if not fields[0] == 'MSG' or not (fields[1] == '3' or fields[1] == '4'):
-                            continue
+                        # Check for valid message type
+                        if fields[0] == 'MSG' and (fields[1] == '3' or fields[1] == '4') and len(fields) > 20:
+                            message_type = fields[0] + fields[1]
+                            aircraft_icao_id, date, timestamp = fields[4], fields[6], fields[7]
 
-                        # Insert the data into the correct table
-                        if fields[1] == '3' and len(fields) > 20:
-                            cursor.execute("INSERT INTO flight_data (message_type, aircraft_icao_id, date, timestamp, altitude, latitude, longitude) VALUES (?, ?, ?, ?, ?, ?, ?)",
-                                           (fields[0] + fields[1], fields[4], fields[6], fields[7], fields[11], fields[14], fields[15]))
-                            print(f"Record added - Message Type: {fields[0] + fields[1]}, Aircraft: {fields[4]}, Altitude: {fields[11]}, Latitude: {fields[14]}, Longitude: {fields[15]}")
-                        elif fields[1] == '4' and len(fields) > 20:
-                            cursor.execute("INSERT INTO navigation_data (message_type, aircraft_icao_id, date, timestamp, speed, heading) VALUES (?, ?, ?, ?, ?, ?)",
-                                           (fields[0] + fields[1], fields[4], fields[6], fields[7], fields[12], fields[13]))
-                            print(f"Record added - Message Type: {fields[0] + fields[1]}, Aircraft: {fields[4]}, Speed: {fields[12]}, Heading: {fields[13]}")
+                            if fields[1] == '3':
+                                altitude, latitude, longitude = fields[11], fields[14], fields[15]
+                                cursor.execute("INSERT INTO flight_data (message_type, aircraft_icao_id, date, timestamp, altitude, latitude, longitude) VALUES (?, ?, ?, ?, ?, ?, ?)",
+                                               (message_type, aircraft_icao_id, date, timestamp, altitude, latitude, longitude))
+                                print(f"Record added - Message Type: {message_type}, Aircraft: {aircraft_icao_id}, Altitude: {altitude}, Latitude: {latitude}, Longitude: {longitude}")
+                            else:
+                                speed, heading = fields[12], fields[13]
+                                cursor.execute("INSERT INTO navigation_data (message_type, aircraft_icao_id, date, timestamp, speed, heading) VALUES (?, ?, ?, ?, ?, ?)",
+                                               (message_type, aircraft_icao_id, date, timestamp, speed, heading))
+                                print(f"Record added - Message Type: {message_type}, Aircraft: {aircraft_icao_id}, Speed: {speed}, Heading: {heading}")
 
-                        # Commit the changes to the database
-                        conn.commit()
-                        time.sleep(10)
-                    except socket.timeout:
-                        print("Socket timeout, waiting for data...")
-                        time.sleep(10)
-                        print('Restarting socket')
-                        sock.close()  # Close the existing socket
-                        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Create a new socket
-                        sock.settimeout(600)  # Set timeout for the new socket
-                        sock.connect((piaware_ip, piaware_port))
-                    except Exception as e:
-                        print(f"Error: {e}")
-            except socket.error as e:
-                print(f"Socket error: {e}")
-            finally:
-                cursor.close()
-                conn.close()
-                sock.close()
+                            conn.commit()
+                            time.sleep(10)
+
+                except socket.timeout:
+                    print("Socket timeout, waiting for data...")
+                    time.sleep(10)
+                    print('Restarting socket')
+                    sock.close()
+                    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                    sock.settimeout(600)
+                    sock.connect((piaware_ip, piaware_port))
+                except Exception as e:
+                    print(f"Error: {e}")
+
         except Exception as e:
             print(f"Outer Error: {e}")
